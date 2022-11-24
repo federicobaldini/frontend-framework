8badd96ddccadd362e9dc991a92e2bb947b783b5
Federico Baldini <federico.baldini7@gmail.com>
2022-11-24 11:24:14 +0100

    Update index.ts
    Added the init() function to insert a DOMElement to an already existing DOM element.

--- framework/index.ts
+++ framework/index.ts
@@ -1,1 +1,15 @@
+type DOMElement = {
+  type: string;
+  template: string;
+};
 
+export const init = (selector: string, component: DOMElement) => {
+  const app = document.querySelector(selector);
+  const newElement = document.createElement(component.type);
+  const newTextContent = document.createTextNode(component.template);
+
+  newElement.append(newTextContent);
+  if (app) {
+    app.append(newElement);
+  }
+};
