cb9ca2466a8176cb898798e7d4f736407b5de23d
Federico Baldini <federico.baldini7@gmail.com>
2022-12-01 15:59:19 +0100

    Update main.ts
    Declared and used an element that implement an onClick event.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { init } from "../framework";
 import { div } from "../framework/element";
+import { onClick } from "../framework/event";
 
 const firstName = "Federico";
 const lastName = "Baldini";
 
-init("#app", div`Hello ${firstName} ${lastName}`);
+const user = (firstName: string, lastName: string) =>
+  div`${onClick(async () => alert(firstName))} Hello ${firstName} ${lastName}`;
+
+init("#app", user(firstName, lastName));
