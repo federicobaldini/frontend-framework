42b13b8ecd08db895db7ae6a2bf7de9b903c0192
Federico Baldini <federico.baldini7@gmail.com>
2022-12-04 11:56:06 +0100

    Update user.ts
    Made code cleaner.

--- src/user.ts
+++ src/user.ts
@@ -10,14 +10,24 @@ const methods = {
   changeName: (state: object, firstName: string) => ({ ...state, firstName }),
 };
 
-// Component template
-const template = (props: {
-  firstName: string;
-  lastName: string;
-  methods: { changeName: (arg0: string) => {} };
+
+/**
+ * Component template.
+ * 
+ * Properties in this case:
+ * 
+ * properties: {
+ *  firstName: string;
+ *  lastName: string;
+ *  methods: { changeName: (arg0: string) => {} };
+ * }
+ */
+const template = (properties: {
+  [key: string]: any;
+  methods: { [key: string]: (newValue: any) => {} };
 }) =>
-  div`${onClick(() =>
-    props.methods.changeName("Matteo")
-  )} Hello ${props.firstName} ${props.lastName}`;
+  div`${onClick(() => properties.methods.changeName("Matteo"))} Hello ${
+    properties.firstName
+  } ${properties.lastName}, click on me to change my state!`;
 
 export const User = createComponent({ template, methods, initialState });
