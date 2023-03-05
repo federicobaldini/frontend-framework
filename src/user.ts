b9e5abad866a374831574ac892d8fb050a17039a
Federico Baldini <federico.baldini7@gmail.com>
2023-03-05 11:57:51 +0100

    Update user.ts
    Updated comments.

--- src/user.ts
+++ src/user.ts
@@ -10,24 +10,15 @@ const methods = {
   changeName: (state: object, firstName: string) => ({ ...state, firstName }),
 };
 
-
-/**
- * Component template.
- * 
- * Properties in this case:
- * 
- * properties: {
- *  firstName: string;
- *  lastName: string;
- *  methods: { changeName: (arg0: string) => {} };
- * }
- */
+// Template function that returns a virtual node that represents the component's UI
 const template = (properties: {
   [key: string]: any;
   methods: { [key: string]: (newValue: any) => {} };
 }) =>
+  // Use the div function to create a div element with an onClick event that calls the changeName method of the component's methods.
   div`${onClick(() => properties.methods.changeName("Matteo"))} Hello ${
     properties.firstName
   } ${properties.lastName}, click on me to change my state!`;
 
+// Creating the User component by calling createComponent function
 export const User = createComponent({ template, methods, initialState });
