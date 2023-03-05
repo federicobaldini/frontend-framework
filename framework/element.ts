497e89eacc0a9d6ef7822c819d3492141d830514
Federico Baldini <federico.baldini7@gmail.com>
2023-03-05 11:57:36 +0100

    Update element.ts
    Updated comments.

--- framework/element.ts
+++ framework/element.ts
@@ -1,11 +1,13 @@
-// h is commonly used to define virtual nodes. The h means hyperscript.
+// h is commonly used to define virtual nodes. The h means hyperscript
 import { h, VNode } from "snabbdom";
 
+// Type alias called DOMElement, which represents a DOM element
 type DOMElement = {
   type: string;
   template: VNode;
 };
 
+// Type for the different types of arguments that can be passed to a template literal
 type LiteralArgument =
   | {
       type: string;
@@ -13,21 +15,24 @@ type LiteralArgument =
     }
   | string;
 
+// Initial state of the reducer function
 const initialState: { template: string; on: { click: () => void } } = {
   template: "",
   on: { click: () => {} }, // This initial state property will be helpful to manage event handlers in template literals
 };
 
+// Define a function that reduces a template literal to a virtual DOM element
 const createReducer =
   (args: Array<LiteralArgument>) =>
   (accumulator: { template: string }, currentString: string, index: number) => {
     const currentArg: LiteralArgument = args[index];
 
-    // Here, it's defined the behavior of an event node and this is where the type is important
+    // If the current argument is an event node, return an object with an event handler
     if (typeof currentArg === "object" && currentArg.type === "event") {
       return { ...accumulator, on: { click: currentArg.click } };
     }
 
+    // Otherwise, append the current string and argument to the template
     return {
       ...accumulator,
       template: accumulator.template + currentString + (args[index] || ""),
@@ -36,6 +41,8 @@ const createReducer =
 
 /**
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+ *
+ * Create an element by passing in a tag name
  *
  * Take this code as example:
  * ```
@@ -49,12 +56,6 @@ const createReducer =
  * "strings" are: [ 'Hello ', ' ', ' !' ]
  *
  * "args" are: [ 'Federico', 'Baldini' ]
- *
- * ___
- *
- * Added a VDOM in the framework so that it will apply DOM modifications only if necessary.
- *
- * The choosen VDOM is snabbdom: https://github.com/snabbdom/snabbdom
  */
 const createElement =
   (tagName: string) =>
@@ -62,14 +63,17 @@ const createElement =
     strings: TemplateStringsArray,
     ...args: Array<LiteralArgument>
   ): DOMElement => {
+    // Reduce the template literal to a virtual DOM element.
     const reducedLiterals = strings.reduce(createReducer(args), initialState);
 
+    // Extract the template and event handler from the reduced template literal
     const template = reducedLiterals.template;
     let on: { click: () => void } = { click: () => {} };
     if ("on" in reducedLiterals) {
       on = reducedLiterals.on;
     }
 
+    // Use the Snabbdom library to create a virtual DOM element with the given tag name, attributes, properties, and event handlers
     return {
       type: "element",
       template: h(tagName, { on }, template), // the second argument concerns attributes, properties and events
